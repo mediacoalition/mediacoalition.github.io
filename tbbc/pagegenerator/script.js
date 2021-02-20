24a0cfb6dcb3c888bdfafd4f2f082d2d93e1c042
George-801 <56756124+George-801@users.noreply.github.com>
2021-02-20 22:01:58 +0000

    woooooooooo done

--- tbbc/pagegenerator/script.js
+++ tbbc/pagegenerator/script.js
@@ -1,14 +1,36 @@
 $(window).on("load", () => {
     $("select[name=template]").on("change", () => {
         if ($("select[name=template]").find("option:selected").attr("name") == "none") {
+            $("#iframe").attr("src", "generalTemplate.html")
             $(".option-stuff").html(`
                 <div class="row">
                     <div class="col">
+                        <h5>Meta (twitter/discord embed stuff)</h5>
                         <label for="title">Page title:</label>
-                        <input name="title" id="title" type="text">
+                        <input name="title" id="title" type="text" placeholder="'- TBBC News' will be added">
+                        <textarea class="w-100" name="desc" id="desc" placeholder="Description"></textarea>
                     </div>
                     <div class="col">
                         <textarea name="md" id="md" class="w-100" placeholder="Page markdown"></textarea>
+                    </div>
+                </div>
+            `)
+        } else if ($("select[name=template]").find("option:selected").attr("name") == "news") {
+            $("#iframe").attr("src", "newsTemplate.html")
+            $(".option-stuff").html(`
+                <div class="row">
+                    <div class="col">
+                        <h5>Meta (twitter/discord embed stuff)</h5>
+                        <label for="title">Page title:</label>
+                        <input name="title" id="title" type="text" placeholder="'- TBBC News' will be added">
+                        <textarea class="w-100" name="desc" id="desc" placeholder="Description"></textarea>
+                    </div>
+                    <div class="col">
+                        <textarea rows="10" name="md" id="md" class="w-100" placeholder="Page markdown">
+# Uber drivers are workers not self-employed, Supreme Court rules
+&lt;small class="credit"&gt;**By Mary-Ann Russon**&lt;br&gt;
+&lt;span style="color: #696969"&gt;Business reporter, BBC News&lt;/span&gt;&lt;/small&gt;                        
+                        </textarea>
                     </div>
                 </div>
             `)
@@ -30,13 +52,19 @@ $(window).on("load", () => {
     </body>
 </html>
 `);
+        preview();
     })
 })
 
 function preview(fromMD) {
     if (fromMD == undefined) fromMD = true;
     $("#iframe").contents().find('meta[property="og:title"]').attr('content', `${$("#title").val()} - TBBC News`);
+    $("#iframe").contents().find('meta[property="twitter:title"]').attr('content', `${$("#title").val()} - TBBC News`);
+    $("#iframe").contents().find('meta[property="og:description"]').attr('content', $("#desc").val());
+    $("#iframe").contents().find('meta[property="twitter:description"]').attr('content', $("#desc").val());
     $("#iframe").contents().find(".page-md").html($("#md").val());
+
+    if (fromMD) $("#iframe")[0].contentWindow.mdToHTML()
 }
 
 function download(filename, text) {
